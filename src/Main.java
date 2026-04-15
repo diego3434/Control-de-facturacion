02e21353505b9868df1b7eff18b5540b379739d6
diego3434 <diegotoapantayanchaguano@gmail.com>
2026-04-15 00:05:24 -0500

    Update Main.java
    cambio de contenedor

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Vendedor vendedor=new Vendedor("Andrea López","Tecnologia",2850.75,8,92);
+        Vendedor v1=new Vendedor("Andrea López","Tecnologia",2850.75,8,92);
 
-         vendedor.mensajeDesempeno();
+         v1.mensajeDesempeno();
 
     }
 }
